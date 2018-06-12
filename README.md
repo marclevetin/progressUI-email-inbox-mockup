84873ea266879b43be2723bb22514a070668c87c
marclevetin <marc@derbyhat.org>
2018-06-12 15:35:26 -0400

    Updated Email summary to better match design.  Added image.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Issues:
 Missing: ./select2-spinner.gif
 Missing: ./select2.png
 Missing: ./select2x2.png
+
+Known issues:
+Avatar in email has a border radius of 50% instead of the overridden 2px.  Not sure why.  Need to investigate.
--- src/components/EmailSummary/EmailSummary.js
+++ src/components/EmailSummary/EmailSummary.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
+import './EmailSummary.css'
+
 const EmailSummary = props => (
-    <div class="page-title-container">
+    <React.Fragment>
+    
+    <div class="page-title-container email">
+    <span class="user-icon avatar email-avatar">
+    <img src="https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg" />
+    </span>
         <div class="page-title-group">
             <h3 class="page-title-sub">{props.sender}</h3>
-            <h5 class="page-title">{props.title}</h5>
+            <h5 class="page-title text-base">{props.title}</h5>
         <p>{props.preheader}</p>
-        <p>border</p>
+        <p>background if active, uses #e9ecef</p>
         </div>
+        
     </div>
+    </React.Fragment>
+    
+    
 )
 
 export default EmailSummary;
