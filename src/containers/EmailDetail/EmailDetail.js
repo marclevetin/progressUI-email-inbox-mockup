ed1c7553d314be601a3c5cffdcb44c22c5291bb0
marclevetin <marc@derbyhat.org>
2018-06-12 14:37:40 -0400

    started email detail

--- src/containers/EmailDetail/EmailDetail.js
+++ src/containers/EmailDetail/EmailDetail.js
@@ -1,17 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class EmailDetail extends Component {
-    state = {
+  state = {};
 
-    }
-
-    render () {
-        return(
-            <div className="col-md-8">
-                This is the email detail
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="col-md-8">
+        <div class="page-title-container">
+          <div class="page-title-group">
+            <h5 class="page-title">Page Title
+                {/* <span class="page-title-group-actions right">
+                    <button class="btn btn-blue btn-ghost btn-xs">Reply</button>
+                    <button class="btn btn-blue btn-ghost btn-xs">Forward</button>
+                    <button class="btn btn-blue btn-ghost btn-xs">Move to</button>
+                </span> */}
+            </h5>
+            <p>From <a>Tio Mitra</a> at Time stamp</p>
+          </div>
+        </div>
+          <hr />
+          <p>Bacon ipsum text</p>
+      </div>
+    );
+  }
 }
 
 export default EmailDetail;
