54db14de7f25ff294d629dda1145624f8f83f5e6
marclevetin <marc@derbyhat.org>
2018-06-12 22:07:29 -0400

    image in EmailSummary is in a closer position than where it was.

--- src/components/EmailSummary/EmailSummary.js
+++ src/components/EmailSummary/EmailSummary.js
@@ -12,8 +12,8 @@ const EmailSummary = props => {
         onClick={() => props.handleClick(props.id)}
         style={backgroundColor}
       >
-        <img className="user-icon avatar email-avatar" src={props.avatar} alt={`Avatar for ${props.sender}`}/> 
         <div className="page-title-group">
+        <img className="user-icon avatar email-avatar" src={props.avatar} alt={`Avatar for ${props.sender}`}/> 
           <h3 className="page-title-sub">{props.sender}</h3>
           <h5 className="page-title text-base">{props.title}</h5>
           <p>{props.preheader}</p>
