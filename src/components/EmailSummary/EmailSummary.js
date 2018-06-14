228286347d5cd07227ad0bf1316eb630f751aab5
marclevetin <marc@derbyhat.org>
2018-06-14 13:03:47 -0400

    improved emailSummary component: picture spacing and text appearance

--- src/components/EmailSummary/EmailSummary.js
+++ src/components/EmailSummary/EmailSummary.js
@@ -3,8 +3,13 @@ import React from "react";
 import "./EmailSummary.css";
 
 const EmailSummary = props => {
-  const backgroundColor = (props.activeEmail === props.id) ? {'backgroundColor': '#e9ecef'} : {'backgroundColor': '#ffffff'};
-  const unReadBorder = (props.unread) ? { 'border-left': '5px solid #72c3fc' } : { 'border-left': '0px solid #72c3fc' };
+  const backgroundColor =
+    props.activeEmail === props.id
+      ? { backgroundColor: "#e9ecef" }
+      : { backgroundColor: "#ffffff" };
+  const unReadBorder = props.unread
+    ? { "border-left": "5px solid #72c3fc" }
+    : { "border-left": "0px solid #72c3fc" };
 
   return (
     <React.Fragment>
@@ -14,10 +19,18 @@ const EmailSummary = props => {
         style={backgroundColor}
       >
         <div className="page-title-group">
-        <img className="user-icon avatar email-avatar" src={props.avatar} alt={`Avatar for ${props.sender}`}/> 
-          <h3 className="page-title-sub">{props.sender}</h3>
-          <h5 className="page-title text-base">{props.title}</h5>
-          <p>{props.preheader}</p>
+          <div>
+            <img
+              className="user-icon avatar email-avatar"
+              src={props.avatar}
+              alt={`Avatar for ${props.sender}`}
+            />
+          </div>
+          <div>
+            <h3 className="text-base bold text-gray no-margin sender">{props.sender}</h3>
+            <h5 className="bold text-base no-margin">{props.title}</h5>
+            <p>{props.preheader}</p>
+          </div>
         </div>
       </div>
     </React.Fragment>
