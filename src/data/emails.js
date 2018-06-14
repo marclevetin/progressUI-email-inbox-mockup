3a85feec16bff0f6d6be79c0e46d71eaca306e18
marclevetin <marc@derbyhat.org>
2018-06-14 14:16:55 -0400

    tweaked email data

--- src/data/emails.js
+++ src/data/emails.js
@@ -3,7 +3,7 @@ const emails = [
     _id: "5b203f39ebd0f472102d50a1",
     index: 0,
     name: "Lawanda Solis",
-    picture: "http://placehold.it/32x32",
+    picture: "https://randomuser.me/api/portraits/thumb/women/99.jpg",
     title:
       "Bacon ipsum is the best lorem ipsum generator",
     message:
@@ -19,7 +19,7 @@ const emails = [
     _id: "5b203f39003fecd4a13c7b62",
     index: 1,
     name: "Gabriela Rhodes",
-    picture: "http://placehold.it/32x32",
+    picture: "https://randomuser.me/api/portraits/thumb/women/18.jpg",
     title: "RE: RE: FWD:  Check out these funny cat videos!",
     message:
       "Et officia commodo sint consequat labore duis sint velit tempor. Incididunt velit deserunt anim culpa culpa proident commodo quis amet dolore ipsum officia consectetur. Dolore tempor mollit qui pariatur aute. Laboris exercitation mollit mollit anim occaecat aute eiusmod consectetur nisi. Id exercitation exercitation duis Lorem mollit quis. Excepteur elit reprehenderit amet dolore officia pariatur consequat laboris voluptate cupidatat in.\r\nUllamco fugiat dolor elit labore deserunt reprehenderit esse anim cupidatat. Aliqua laboris anim aliqua quis deserunt officia nostrud enim mollit ea proident velit Lorem eu. Fugiat dolor minim deserunt ea nostrud elit ea non ut ullamco dolore irure occaecat eu. Eiusmod id esse et sint exercitation est reprehenderit. Pariatur laboris non aute incididunt dolore eu dolore exercitation laboris sint aliquip anim. Officia qui cupidatat commodo qui mollit velit aliqua ullamco duis occaecat anim magna dolore mollit.\r\nSunt esse consectetur eiusmod reprehenderit. Esse mollit consectetur pariatur occaecat id eu ipsum pariatur deserunt. Sit nisi dolore et laboris enim veniam. Incididunt incididunt anim consequat exercitation occaecat reprehenderit laborum magna excepteur exercitation ullamco et consectetur. Nostrud dolor ea tempor sunt laboris cupidatat ipsum irure anim incididunt. Qui mollit excepteur est consectetur. Consequat ipsum dolor minim amet est aliquip exercitation aliquip Lorem nisi ipsum nisi fugiat irure.\r\n",
@@ -34,7 +34,7 @@ const emails = [
     _id: "5b203f394e3ae040eea3c694",
     index: 2,
     name: "Kara Travis",
-    picture: "http://placehold.it/32x32",
+    picture: "https://randomuser.me/api/portraits/thumb/women/63.jpg",
     title:
       "Job opportunity: Want to work in Somerville?",
     message:
@@ -50,7 +50,7 @@ const emails = [
     _id: "5b203f3928cf7b6a8512be8b",
     index: 3,
     name: "Lynette Lang",
-    picture: "http://placehold.it/32x32",
+    picture: "https://randomuser.me/api/portraits/thumb/women/81.jpg",
     title: "Hipster ipsum was cool before it was cool",
     message:
       "Lorem ipsum dolor amet four loko wolf PBR&B, DIY church-key selfies portland brooklyn tumblr vinyl synth gochujang photo booth man bun celiac. Air plant freegan man braid letterpress. Palo santo seitan keffiyeh freegan skateboard normcore put a bird on it narwhal four dollar toast. Yuccie drinking vinegar narwhal, banjo bicycle rights master cleanse meggings ramps poutine jean shorts. Chambray enamel pin food truck banh mi, tote bag viral poke tumeric selfies hot chicken small batch taiyaki cardigan adaptogen DIY. Sriracha health goth listicle flexitarian flannel disrupt.\r\n",
@@ -64,8 +64,8 @@ const emails = [
   {
     _id: "5b203f39d08199fd66da7ee1",
     index: 4,
-    name: "Yahoo! Finance",
-    picture: "http://placehold.it/32x32",
+    name: "Rob Alexander",
+    picture: "https://randomuser.me/api/portraits/thumb/men/1.jpg",
     title: "How to protect your investments in uncertain times",
     message:
       "Do irure duis sunt veniam culpa Lorem. Laboris tempor laborum duis duis ut enim excepteur sint sint consequat. Labore nisi pariatur amet eiusmod id qui aliquip qui adipisicing pariatur sit nulla elit ut.\r\nExercitation ut amet qui officia laborum ipsum minim. Consequat ullamco officia voluptate ipsum officia veniam dolore eiusmod eiusmod. Nulla ipsum non eu voluptate deserunt officia ipsum consequat labore qui voluptate consequat excepteur proident. Eiusmod ex nostrud minim culpa consequat incididunt aliqua dolore consectetur.\r\nAd aliquip esse proident veniam qui id sint mollit laboris. Incididunt consectetur ullamco officia nulla excepteur et aliqua commodo non amet. Eu Lorem veniam ad ut qui amet incididunt aliquip et ex id. Pariatur deserunt sit nostrud consequat qui cupidatat. Duis laborum deserunt Lorem nulla cillum aute amet officia esse. Ea nulla voluptate exercitation elit consequat consectetur officia.\r\n",
@@ -79,10 +79,10 @@ const emails = [
   {
     _id: "5b203f395bbe824f958aeb60",
     index: 5,
-    name: "Google Calendar",
-    picture: "http://placehold.it/32x32",
+    name: "Nina Fielder",
+    picture: "https://randomuser.me/api/portraits/thumb/women/72.jpg",
     title:
-      "Daily agenda for Neil and Eileen Down",
+      "Fwd: Support the MSPCA",
     message:
       "Aliqua magna est commodo ullamco in sit. Enim veniam do laboris elit. Consectetur duis incididunt commodo aute cillum irure et duis Lorem consectetur dolor. Minim ad amet consequat proident. Reprehenderit occaecat et amet nostrud et sunt culpa deserunt dolor id cillum.\r\nEx amet ullamco nostrud occaecat ipsum. Lorem nisi fugiat sunt Lorem excepteur aute magna id id quis voluptate esse. Aute ut sunt voluptate eu minim.\r\nEa esse fugiat labore ut dolor eu mollit. Eu quis cillum exercitation tempor veniam deserunt in cillum proident aliquip cupidatat deserunt veniam proident. Sint voluptate ut deserunt aute.\r\n",
     dateSent: "2016-05-07T04:44:44 +04:00",
@@ -95,9 +95,9 @@ const emails = [
   {
     _id: "5b203f398123f0b5ce06726b",
     index: 6,
-    name: "Apartments.com",
-    picture: "http://placehold.it/32x32",
-    title: "Rental alert for Ivanna Livdarr",
+    name: "Haj Okuda",
+    picture: "https://randomuser.me/api/portraits/thumb/men/11.jpg",
+    title: "Rental alert for Chelmsford, MA",
     message:
       "Consequat ullamco magna fugiat aliqua commodo duis in aliqua. Occaecat in culpa exercitation et culpa deserunt sit tempor aliquip. Duis cupidatat magna minim id ea sint incididunt laboris eiusmod amet ullamco tempor non. Deserunt in ut sit exercitation aliquip proident consectetur occaecat quis. Labore consectetur do aliqua ad laborum anim id minim officia elit aliqua velit.\r\nSint aute aute reprehenderit voluptate dolor cupidatat. Nulla magna anim commodo mollit anim anim mollit magna. Fugiat est officia ex irure pariatur. Aliqua ea culpa ut nulla aliquip quis.\r\nCulpa ullamco exercitation proident eu tempor enim tempor aliquip qui excepteur aliquip. Veniam ea esse sunt est ex elit incididunt elit aute consequat officia. Amet ullamco ex anim laboris. Laboris id consectetur adipisicing eiusmod laborum et adipisicing commodo amet mollit. Magna officia tempor proident amet do sunt ad. Amet id laboris aliquip quis voluptate eiusmod dolor pariatur enim magna nostrud sint.\r\n",
     dateSent: "2017-01-22T07:28:45 +05:00",
@@ -111,7 +111,7 @@ const emails = [
     _id: "5b203f39c44e0101bf696e98",
     index: 7,
     name: "Duffy Johnston",
-    picture: "http://placehold.it/32x32",
+    picture: "https://randomuser.me/api/portraits/thumb/men/81.jpg",
     title: "Can you get some milk on your way home?",
     message:
       "Enim qui anim eiusmod consequat dolore consequat et sunt laborum magna anim ut nostrud. Elit eiusmod do amet aliqua ut dolore minim voluptate commodo dolor sunt. Sint enim velit reprehenderit voluptate proident laborum ex reprehenderit eiusmod aute. Consectetur anim dolor excepteur quis est ullamco. Dolore esse minim aliqua commodo ullamco quis. Qui officia dolor minim occaecat qui.\r\nLorem minim non minim incididunt incididunt dolore enim nisi esse quis et non. Sint fugiat amet qui ipsum non ea cillum consectetur eiusmod amet. Amet occaecat id eiusmod voluptate sint anim commodo fugiat cillum velit do aute qui excepteur. Sit reprehenderit ex ea commodo nulla deserunt. Elit aute excepteur ullamco pariatur Lorem enim aute. Consequat aute deserunt amet cupidatat deserunt mollit Lorem exercitation. Incididunt nostrud eiusmod ipsum irure consequat duis ipsum deserunt ex qui elit officia exercitation amet.\r\nCupidatat culpa minim non irure officia adipisicing enim ea elit deserunt magna exercitation ullamco. Dolore excepteur sit nostrud exercitation magna magna pariatur in ullamco. Ipsum qui laborum mollit deserunt id labore ad quis proident incididunt laboris cillum cupidatat commodo.\r\n",
@@ -126,7 +126,7 @@ const emails = [
     _id: "5b203f3996d1a98c10fbe24f",
     index: 8,
     name: "Meghan Browning",
-    picture: "http://placehold.it/32x32",
+    picture: "https://randomuser.me/api/portraits/thumb/women/49.jpg",
     title: "NGPVAN is a really great company",
     message:
       "Sint reprehenderit do anim officia nisi dolore ut cupidatat. Aute esse nostrud labore sint adipisicing. Est est adipisicing ea id laborum cupidatat laboris non occaecat cupidatat do duis. Velit proident ipsum enim est enim veniam consequat dolor ipsum amet culpa irure.\r\nVoluptate magna laborum minim elit cillum ea cillum officia quis voluptate. Incididunt aliqua aliquip et enim exercitation pariatur eiusmod nostrud. Commodo ad voluptate reprehenderit minim enim ex magna ea tempor minim occaecat magna laboris. Elit sunt quis labore occaecat irure id cupidatat. Proident voluptate minim sit cillum do fugiat fugiat. Quis aute aliquip do sit. Sunt veniam et Lorem aliqua officia officia.\r\nVeniam mollit magna ea aliquip enim id in ex duis eiusmod exercitation enim. Consequat ut ex commodo incididunt dolore aute. Eiusmod adipisicing occaecat qui dolor in laboris dolore. Irure excepteur occaecat Lorem incididunt nisi ut reprehenderit velit dolor id aliqua incididunt. Do laborum id excepteur dolore ea consequat laboris cillum. Irure et ad aute sunt duis esse irure non in aliqua enim sunt proident. Eiusmod ad aliquip et velit do.\r\n",
